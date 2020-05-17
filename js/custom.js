f11ad74bd78986a5a38bdbf99d4c4166297c34fe
mikel <mikel@PC-OXY>
2020-05-17 12:01:11 +0300

    Merge origin/master
    Conflicts:
    	js/script.js

--- js/custom.js
+++ js/custom.js
@@ -202,3 +202,64 @@ function createPortfolio3() {
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+
+/* 
+<div class="accordion md-accordion" id="accordionEx" role="tablist" aria-multiselectable="true">
+
+<!-- Accordion card -->
+<div class="card">
+  <!-- Card header -->
+  <div class="card-header" role="tab" id="headingOne1">
+    <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne1" aria-expanded="true"
+      aria-controls="collapseOne1">
+      <h5 class="mb-0"> Animācijas </h5>
+    </a>
+  </div>
+  <!-- Card body -->
+  <div id="collapseOne1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1"
+    data-parent="#accordionEx">
+    <div class="card-body">
+      bla bla bla.... teksts
+    </div>
+  </div>
+</div>
+
+<!-- Accordion card -->
+<div class="card">
+  <!-- Card header -->
+  <div class="card-header" role="tab" id="headingTwo2">
+    <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2"
+      aria-expanded="false" aria-controls="collapseTwo2">
+      <h5 class="mb-0">Pielāgoti attēli</h5>
+    </a>
+  </div>
+  <!-- Card body -->
+  <div id="collapseTwo2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2"
+    data-parent="#accordionEx">
+    <div class="card-body">
+      bla bla bla.... teksts
+    </div>
+  </div>
+</div>
+
+<!-- Accordion card -->
+<div class="card">
+  <!-- Card header -->
+  <div class="card-header" role="tab" id="headingThree3">
+    <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseThree3"
+      aria-expanded="false" aria-controls="collapseThree3">
+      <h5 class="mb-0">Interaktīvs dizains</h5>
+    </a>
+  </div>
+  <!-- Card body -->
+  <div id="collapseThree3" class="collapse" role="tabpanel" aria-labelledby="headingThree3"
+    data-parent="#accordionEx">
+    <div class="card-body">
+      bla bla bla.... teksts
+    </div>
+  </div>
+</div>
+
+</div>
+*/
